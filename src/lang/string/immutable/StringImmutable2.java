dd1b78dac34064ab411cbd539151f374f0c46e55
kanghm <kanghm0513@khu.ac.kr>
2026-01-26 13:22:07 +0900

    24. String 클래스 - 불변 객체

---
+++ src/lang/string/immutable/StringImmutable2.java
@@ -0,0 +1,10 @@
+package lang.string.immutable;
+
+public class StringImmutable2 {
+    public static void main(String[] args) {
+        String str = "hello";
+        String str2 = str.concat(" java");
+        System.out.println("str = " + str);// String은 불변 객체
+        System.out.println("str2 = " + str2);
+    }
+}
