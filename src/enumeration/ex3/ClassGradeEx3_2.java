6d068b472db31a1ea31b5805a28d43a86f5b86ee
kanghm <kanghm0513@khu.ac.kr>
2026-02-03 17:43:38 +0900

    47. 열거형 - Enum Type

---
+++ src/enumeration/ex3/ClassGradeEx3_2.java
@@ -0,0 +1,16 @@
+package enumeration.ex3;
+
+
+public class ClassGradeEx3_2 {
+    public static void main(String[] args) {
+
+
+        int price = 10_000;
+
+        DiscountService discountService = new DiscountService();
+//        ClassGrade newClassGrade = new ClassGrade();
+//        int result = discountService.discount(newClassGrade, price);
+//        System.out.println("newClassGrade 등급의 할인 금액: " + result);
+    }
+
+}
