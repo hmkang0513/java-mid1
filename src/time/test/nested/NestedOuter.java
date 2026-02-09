d28ced417950e66b4a0e147392abaa36f4341411
kanghm <kanghm0513@khu.ac.kr>
2026-02-09 14:49:36 +0900

    69. 정적 중첩 클래스

---
+++ src/time/test/nested/NestedOuter.java
@@ -0,0 +1,23 @@
+package time.test.nested;
+
+public class NestedOuter {
+    private static int outClassValue = 3;
+    private int outInstatnceValue =2;
+
+    static class Nested{
+        private int nestedInstanceValue = 1;
+
+        public void print(){
+            // 자신의 멤버에 접근
+            System.out.println(nestedInstanceValue);
+
+            // 바깥 클래스의 인스턴스 멤버에 접근
+//            System.out.println(outInstanceValue);
+
+            // 바깥 클래스의 클래스 멤버에는 접근할 수 있다. private도 접근 가능
+            System.out.println(NestedOuter.outClassValue);
+            // 같은 거임
+            System.out.println(outClassValue);
+        }
+    }
+}
